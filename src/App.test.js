ff1f453f745e28e13669bf56b191052ea7adfe5a
GAguilar208 <gaguilar208.accessibility@gmail.com>
2022-12-29 18:12:34 -0700

    feat: modal in react with focus

--- src/App.test.js
+++ src/App.test.js
@@ -1,6 +1,5 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
-import Modal from '../src/components/Modal'
 
 test('renders text on top of button without crashing', () => {
   render(<App />);
